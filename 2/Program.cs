e1550d60b889a909ae443996b639ec2019c3403b
brokerr1 <solovievdima1@gmail.com>
2022-05-25 21:27:53 +0300

    2

--- 2/Program.cs
+++ 2/Program.cs
@@ -6,3 +6,12 @@
 9012 -> 12
 */
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 0)
+{
+    result = result + (number % 10);
+    number = number / 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {result}");
