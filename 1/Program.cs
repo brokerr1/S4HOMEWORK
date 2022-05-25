ec465d9a745405b8af0a46a31c48ca92822424b3
brokerr1 <solovievdima1@gmail.com>
2022-05-25 21:19:29 +0300

    1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int NatStep(int a, int b)
+{
+    return Convert.ToInt32(Math.Pow(a,b));
+}
+int result = NatStep(numberA, numberB);
+Console.WriteLine($"Число A в степени B = {result}");
